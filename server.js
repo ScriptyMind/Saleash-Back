a44340d881e5d9528e582661a7b784216e8563b9
Taha Baz <etahabaz@gmail.com>
2020-08-25 17:49:35 +0100

    delete index.js and change the port to 5000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.status(200).send('HELLO WORLD');
 });
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   // eslint-disable-next-line no-console
   console.log('Server IS ON');
 });
