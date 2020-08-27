203edd59f1bd7869a993cbbaf2ef98740c0452e1
Taha Baz <etahabaz@gmail.com>
2020-08-27 18:05:24 +0100

    add the Agent Model

---
+++ models/Agent.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const agentSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'user',
+      required: true,
+      unique: true,
+    },
+  },
+);
+
+const Agent = mongoose.model('agent', agentSchema);
+module.exports = Agent;
