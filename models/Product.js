9f1ee6dc6923613ecaed2461e5618631884e5105
Taha Baz <etahabaz@gmail.com>
2020-08-30 21:05:51 +0100

    add Product model

---
+++ models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    }
+  }
+);
+
+const Product = mongoose.model('product', productSchema);
+module.exports = Product;
