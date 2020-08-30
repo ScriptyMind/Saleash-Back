674ebb6166dc14d6d73353e8824bfc30a1b49066
MrKey <miftah1997mustapha@gmail.com>
2020-08-30 20:29:01 +0100

    Minor changes

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const connect = () =>
-  mongoose
-    .connect(process.env.DB_CONNECTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch((err) => console.log(err));
+const connect = () => mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch((err) => console.log(err));
 
 module.exports = connect;
