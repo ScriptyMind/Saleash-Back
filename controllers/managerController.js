d61e441b39596c2a889a1fe44dba7d84e6298575
Taha Baz <etahabaz@gmail.com>
2020-09-06 14:52:04 +0100

    add createInvitation in managerController

---
+++ controllers/managerController.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const managerService = require('../services/managerService');
+const invitationService = require('../services/invitationService');
+
+const createInvitation = async (req/* , res */) => {
+  const invitation = {
+    manager: req.user.id,
+    company: req.user.company,
+    link: `${req.headers.host}/api/invitation/${mongoose.Types.ObjectId()}`
+  };
+  try {
+    const created = await invitationService.createOne(invitation);
+    return created;
+    // res.status(201).json({ data: created });
+  } catch (e) {
+    console.log(e);
+    // res.status(400).end();
+  }
+};
+
+// const connect = require('../db/connect');
+// const start = async () => {
+//   try {
+//     await connect();
+//     console.log("connected to database");
+//     const created = await createInvitation({
+//       user: {
+//         id: mongoose.Types.ObjectId(),
+//         company: 'LENOVO',
+//       },
+//       headers: {
+//         host: 'https://localhost:3000'
+//       }
+//     });
+//     console.log(created);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+// start();
+
+module.exports = {
+  createInvitation,
+};
