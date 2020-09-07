ebdd18d91ee75ba33e40da1a04d2140e9acf62b1
Taha Baz <etahabaz@gmail.com>
2020-09-07 15:24:30 +0100

    fix bug in managerController

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const managerService = require('../services/managerService');
 const invitationService = require('../services/invitationService');
 
-const createInvitation = async (req/* , res */) => {
+const createInvitation = async (req, res) => {
   const invitation = {
     manager: req.user.id,
     company: req.user.company,
@@ -10,29 +9,31 @@ const createInvitation = async (req/* , res */) => {
   };
   try {
     const created = await invitationService.createOne(invitation);
-    return created;
-    // res.status(201).json({ data: created });
+    res.status(201).json({ data: created });
   } catch (e) {
     console.log(e);
-    // res.status(400).end();
+    res.status(400).end();
   }
 };
 
 // const connect = require('../db/connect');
+
 // const start = async () => {
 //   try {
 //     await connect();
-//     console.log("connected to database");
-//     const created = await createInvitation({
-//       user: {
-//         id: mongoose.Types.ObjectId(),
-//         company: 'LENOVO',
-//       },
-//       headers: {
-//         host: 'https://localhost:3000'
-//       }
-//     });
-//     console.log(created);
+//     // console.log("connected to database");
+//     // const created = await createInvitation({
+//     //   user: {
+//     //     id: mongoose.Types.ObjectId(),
+//     //     company: 'HP',
+//     //   },
+//     //   headers: {
+//     //     host: 'https://localhost:3000'
+//     //   }
+//     // });
+//     // console.log(created);
+//     // const newy = await Manager.find().exec();
+//     // console.log(newy);
 //   } catch (e) {
 //     console.log(e);
 //   }
