7a4f00b7acaaf3b1301f6f517b1ae9b508adf8e5
Taha Baz <etahabaz@gmail.com>
2020-09-09 15:40:25 +0100

    add deleteDriver in the managerService

--- services/managerService.js
+++ services/managerService.js
@@ -1,4 +1,12 @@
 const Manager = require('../models/Manager');
 const parentService = require('./parentService');
 
-module.exports = parentService(Manager);
+const deleteDriver = async (managerId, driverId) => {
+  const manager = await parentService(Manager).updateOne(
+    { _id: managerId }, { $pullAll: { drivers: [driverId] } }
+  );
+  if (!manager) Promise.reject(new Error('Error occured'));
+  Promise.resolve(manager);
+};
+
+module.exports = { ...parentService(Manager), deleteDriver };
