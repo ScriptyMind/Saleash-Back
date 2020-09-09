d35265a5f35a6f4e16647aabe257fd7a62add3e5
Taha Baz <etahabaz@gmail.com>
2020-09-09 17:53:00 +0100

    add getAllDrivers to managerService

--- services/managerService.js
+++ services/managerService.js
@@ -6,13 +6,13 @@ const deleteDriver = async (managerId, driverId) => {
     { _id: managerId }, { $pullAll: { drivers: [driverId] } }
   );
   if (!manager) Promise.reject(new Error('Error occured'));
-  Promise.resolve(manager);
+  return Promise.resolve(manager);
 };
 
 const getAllDrivers = async (managerId) => {
   try {
     const drivers = await parentService(Manager).getPopulate({ _id: managerId }, 'drivers');
-    Promise.resolve(drivers);
+    return Promise.resolve(drivers);
   } catch (e) {
     Promise.reject(e);
   }
