6e54b6459a7e7e500cfdfe4d2d66a1eebdacbde2
Taha Baz <etahabaz@gmail.com>
2020-09-09 18:59:19 +0100

    fix bug

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -51,6 +51,23 @@ const getDrivers = async (req, res) => {
   }
 };
 
+const connect = require('../db/connect');
+const parentService = require('../services/parentService');
+const User = require('../models/User');
+
+const start = async () => {
+  try {
+    await connect();
+    console.log("connected");
+    const manager = await parentService(User).getAll();
+    console.log(manager);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
+
 module.exports = {
   createInvitation,
   deleteInvitation,
